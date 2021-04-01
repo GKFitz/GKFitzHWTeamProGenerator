789203395ef9ac5465a787d3272ce9488fef2151
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-01 03:05:21 -0400

    added the inquirer questions

--- index.js
+++ index.js
@@ -7,10 +7,110 @@ const Engineer= require("./lib/Engineer.js");
 const Intern= require("./lib/Intern.js");
 const Manager= require("./lib/Manager.js");
 
-const employees = [];
+const team = [];
 
 
-const roger= new Employee("roger", 1, "rogerDodger@test.com");
+// const roger= new Employee("roger", 1, "rogerDodger@test.com");
 
-console.log(roger)
+// console.log(roger)
+
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "Welcome to the team!!!"
+        },
+        {
+            type: "input",
+            message: "What is the team manager's name?",
+            name: "name"
+        },
+        {
+            type: "number",
+            message: "What is the team manager's employee ID?",
+            name: "id"
+
+        },
+        {
+            type: "input",
+            message: "What is the team manager's email address?",
+            name: "email"
+        }, 
+        {
+            type: "number",
+            message: "What is the team manager's office number?",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "Would you like to add an engineer or an intern to your team?",
+            choices: [
+                "Engineer",
+                "Intern"
+            ],
+            name: "role"
+
+        }
+
+    ])
+    .then(function(){
+            
+    })
+    
+}
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name"
+        },
+        {
+            type: "number",
+            message: "What is the engineer's employee ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email address?",
+            name: "email"
+        }, 
+        {
+            type: "number",
+            message: "What is the engineer's github?",
+            name: "github"
+        }
+
+    ])
+    .then(function())
+}
+
+function addIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            type: "number",
+            message: "What is the interns's employee ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the intern's email address?",
+            name: "email"
+        }, 
+        {
+            type: "input",
+            message: "What school does the intern attend?",
+            name: "school"
+        }
+
+    ])
+    .then function(){
+        
+    })
+}
+
 
