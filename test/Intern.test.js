2d58000dbbff7e25d65dc9ea2b5a0f17fd6e00fa
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-01 01:07:45 -0400

    set up the intern test; all the tests pass

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,3 +1,12 @@
 const Intern= require("../lib/Intern.js");
 
+test(" can create an instance of an engineer",() => {
+    const roger= new Intern();
+    expect(typeof roger).toBe("object")
+})
 
+test("get the email of an employee with the method",() => {
+    const school= "Columbia";
+    const roger= new Intern("name", 1, "email", school);
+    expect(roger.getSchool()).toBe(school);
+})
