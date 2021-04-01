9c2e9dd7f68d13f4e5c0d28a1177c94951da11f7
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-01 00:51:00 -0400

    set up the Manager object and test. passed

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,3 +1,13 @@
 const Manager= require("../lib/Manager.js");
 
 
+test(" can create an instance of an manager",() => {
+    const roger= new Manager();
+    expect(typeof roger).toBe("object")
+})
+
+test("get the office number of an manager employee with the method",() => {
+    const officeNumber= 10;
+    const roger= new Manager("name", 1, "email", officeNumber);
+    expect(roger.getOfficeNumber()).toBe(officeNumber);
+})
