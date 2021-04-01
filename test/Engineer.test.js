a5327885a4d05beaa8cc44a7b9af10f55b985a3a
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-01 00:55:44 -0400

    set up the engineer test: passed

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,3 +1,12 @@
 const Engineer= require("../lib/Engineer.js");
 
+test(" can create an instance of an engineer",() => {
+    const roger= new Engineer();
+    expect(typeof roger).toBe("object")
+})
 
+test("get the email of an employee with the method",() => {
+    const github= "Roger123";
+    const roger= new Engineer("name", 1, "email", github);
+    expect(roger.getGithub()).toBe(github);
+})
